5aa45292d41278284d6cdea499d75c6a8d721a53
Alex Hamlin <alex@alexhamlin.co>
2026-01-22 19:13:55 -0800

    Remove the xt::detect module
    This used to include a huge "Discard" type that old implementations of
    YAML and TOML detection relied on. I removed that type when Rust started
    linting it as dead, but didn't think much of the module having a single
    function left.

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,6 @@ use std::io::{self, Read, Write};
 
 use serde::{de, ser};
 
-mod detect;
 mod error;
 mod input;
 mod json;
@@ -135,7 +134,7 @@ where
 	fn translate(&mut self, mut input: input::Handle<'_>, from: Option<Format>) -> Result<()> {
 		let from = match from {
 			Some(format) => format,
-			None => match detect::detect_format(&mut input)? {
+			None => match Format::detect(&mut input)? {
 				Some(format) => format,
 				None => return Err("unable to detect input format".into()),
 			},
@@ -276,5 +275,41 @@ impl fmt::Display for Format {
 			Self::Toml => "TOML",
 			Self::Yaml => "YAML",
 		})
+	}
+}
+
+impl Format {
+	/// Detects the input format by trying each known format and selecting the first
+	/// one that works.
+	fn detect(input: &mut input::Handle) -> io::Result<Option<Format>> {
+		// As a binary format, we expect MessagePack to be more restrictive than any
+		// text format. Detection of MessagePack inputs is limited to collection
+		// types; see comments in the implementation for details.
+		if crate::msgpack::input_matches(input.borrow_mut())? {
+			return Ok(Some(Format::Msgpack));
+		}
+
+		// We expect JSON to be more restrictive than other text formats. For
+		// example, a "#" comment at the start of a document could be TOML or YAML,
+		// but definitely not JSON.
+		if crate::json::input_matches(input.borrow_mut())? {
+			return Ok(Some(Format::Json));
+		}
+
+		// YAML is actually less restrictive than TOML, but we want to try it first
+		// since it supports streaming input (which means that detection may require
+		// less buffering). Detection of YAML inputs is limited to collection types;
+		// see comments in the implementation for details.
+		if crate::yaml::input_matches(input.borrow_mut())? {
+			return Ok(Some(Format::Yaml));
+		}
+
+		// Finally, TOML is the only input format that must fully buffer input
+		// before parsing.
+		if crate::toml::input_matches(input.borrow_mut())? {
+			return Ok(Some(Format::Toml));
+		}
+
+		Ok(None)
 	}
 }
